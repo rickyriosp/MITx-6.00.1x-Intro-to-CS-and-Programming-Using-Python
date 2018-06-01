192a7b2ab12c595241ae2964497c52701f092af7
nas7ybruises <riki.rios91@gmail.com>
2018-06-01 10:21:18 -0400

    Initial commit

---
+++ 3 - Guess my number.py
@@ -0,0 +1,25 @@
+low = 0
+high = 100
+ans = (low+high)//2
+user = ''
+
+print('Please think of a number between 0 and 100!')
+while user != 'c':
+    print('Is your secret number', str(ans) + '?')
+    user = input("Enter 'h' to indicate the guess is too high. Enter 'l' to " \
+             "indicate the guess is too low. Enter 'c' to indicate I guessed " \
+             "correctly. ")
+    while user not in ['l', 'h', 'c']:
+        print('Sorry, I did not understand your input.')
+        print('Is your secret number', str(ans) + '?')
+        user = input("Enter 'h' to indicate the guess is too high. Enter 'l' to " \
+             "indicate the guess is too low. Enter 'c' to indicate I guessed " \
+             "correctly. ")
+        
+    if user == 'l':
+        low = ans
+    elif user == 'h':
+        high = ans
+    ans = (low+high)//2
+    
+print('Game over. Your secret number was:', ans)
