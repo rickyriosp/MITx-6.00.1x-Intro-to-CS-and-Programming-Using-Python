8d780f240f73e1bd4af8b722ac9036807a5d1e9c
nas7ybruises <riki.rios91@gmail.com>
2018-06-03 13:20:39 -0400

    Add Towers of Hannoi

---
+++ 4 - Towers of Hannoi.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  3 13:08:42 2018
+
+@author: nas7ybruises
+"""
+
+def printMove(fr, to):
+    print('move from ' + str(fr) + ' to ' + str(to))
+
+def Towers(n, fr, to, spare):
+    if n == 1:
+        printMove(fr, to)
+    else:
+        Towers(n-1, fr, spare, to)
+        Towers(1, fr, to, spare)
+        Towers(n-1, spare, to, fr)
+
+print(Towers(4, 'P1', 'P2', 'Spare'))
