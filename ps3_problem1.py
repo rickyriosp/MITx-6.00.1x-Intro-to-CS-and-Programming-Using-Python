afaed05bb46242e34ed697f0c782d894165fa5a1
nas7ybruises <riki.rios91@gmail.com>
2018-07-02 09:34:10 -0400

    Add ps3_problem1

---
+++ ps3_problem1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 16 20:30:46 2018
+
+@author: nas7ybruises
+"""
+
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    return sum([0 if i in lettersGuessed else 1 for i in secretWord]) == 0
