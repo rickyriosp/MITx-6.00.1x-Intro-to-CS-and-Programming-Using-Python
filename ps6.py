f427f7eb40577bed01c3f6635d515a2d342a7ac3
nas7ybruises <riki.rios91@gmail.com>
2018-07-18 21:02:08 -0300

    Add ps6_problem3

--- ps6.py
+++ ps6.py
@@ -203,7 +203,7 @@ class CiphertextMessage(Message):
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
         '''
-        pass #delete this line and replace with your code here
+        Message.__init__(self, text)
 
     def decrypt_message(self):
         '''
@@ -221,7 +221,18 @@ class CiphertextMessage(Message):
         Returns: a tuple of the best shift value used to decrypt the message
         and the decrypted message text using that shift value
         '''
-        pass #delete this line and replace with your code here
+        max = 0
+        shift = 0
+        for i in range(27):
+            new = self.apply_shift(i)
+            for word in new.split():
+                cur = 0
+                if is_word(self.valid_words, word):
+                    cur += 1
+            if cur > max:
+                max = cur
+                shift = i
+        return (shift, self.apply_shift(shift))
 
 
 #Example test case (PlaintextMessage)
