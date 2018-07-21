6c382095097399659a1aeabfc52184ce94b87f80
nas7ybruises <riki.rios91@gmail.com>
2018-07-21 16:19:07 -0300

    Add MergeSort

---
+++ 12 - MergeSort.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 21 16:11:26 2018
+
+@author: nas7ybruises
+"""
+
+def merge_sort(L):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = len(L)//2
+        left = merge_sort(L[:middle])
+        right = merge_sort(L[middle:])
+        return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i,j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+
+test = [2, 3, 4, 7, 1, 0, 9, 8, 5, 6]
